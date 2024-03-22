2a9aa041a925a625bbe26d604587c33fb6236559
Johan Nyman <johan.nyman97@gmail.com>
2024-03-22 13:50:48 +0100

    sätter group recoil-staten till alla grupper vid login/refresh

--- frontend/src/Views/HomeView.jsx
+++ frontend/src/Views/HomeView.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Aside from "../Components/Aside";
+import { useSetRecoilState } from "recoil";
+import groupApi from "../api/group";
+import { allGroupsAtom } from "../Recoil/atoms";
+import { useCookies } from "react-cookie";
 
 function HomeView({ onLogout }) {
+	const setGroups = useSetRecoilState(allGroupsAtom);
+	const [cookies, setCookie, removeCookie] = useCookies(["AccessToken"]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				if (cookies.AccessToken) {
+					await groupApi.getAll(setGroups, cookies.AccessToken);
+				}
+			} catch (error) {
+				console.error("Error fetching groups:", error.message);
+			}
+		};
+
+		fetchData();
+	}, [setGroups, cookies.AccessToken]);
+
 	return (
 		<div className="w-screen h-screen flex space-y-4 bg-primaryBg text-white">
 			<Aside onLogout={onLogout} />
-			<div className=" flex flex-col justify-center items-center w-full">
+			<div className="flex flex-col justify-center items-center w-full">
 				<h2>HomeView</h2>
 			</div>
 		</div>
