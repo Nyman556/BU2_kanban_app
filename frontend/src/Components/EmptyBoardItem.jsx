3a18749001c5b6844b5ee69deb0b3cfcd880173f
Johan Nyman <johan.nyman97@gmail.com>
2024-03-23 02:43:00 +0100

    döpte om komponenten

--- frontend/src/Components/EmptyBoardItem.jsx
+++ frontend/src/Components/EmptyBoardItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function CategoryEmpty() {
+function EmptyBoardItem() {
 	return (
 		<span className="flex flex-col bg-secondaryBg rounded-lg max-w-48 p-4 mt-4 space-y-4 text-base text-center">
 			None
@@ -8,4 +8,4 @@ function CategoryEmpty() {
 	);
 }
 
-export default CategoryEmpty;
+export default EmptyBoardItem;
