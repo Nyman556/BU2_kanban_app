2fc60a1638aa36414eb88385b8eac4364f2cdabd
Johan Nyman <johan.nyman97@gmail.com>
2024-03-25 00:48:31 +0100

    fixade höjden

--- frontend/src/Components/Task.jsx
+++ frontend/src/Components/Task.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import { FiX, FiChevronsRight } from "react-icons/fi";
 function Task({ task }) {
 	return (
-		<div className="flex flex-col bg-secondaryBg rounded-lg min-h-40 max-w-48 p-4 mt-4 h-full justify-between">
-			<div>
+		<div className="flex flex-col bg-secondaryBg rounded-lg h-fit max-w-48 p-4 mt-4 justify-between">
+			<div className="mb-4">
 				<p className=" text-lg capitalize">{task.title}</p>
 				<span className=" text-gray-500 text-base">{task.description}</span>
 			</div>
