1019fe775edf6b4800e3c74b4d2e2f49d454bed1
Johan Nyman <johan.nyman97@gmail.com>
2024-03-25 00:48:14 +0100

    fixade positioneringen

--- frontend/src/Components/OutcomeMessage.jsx
+++ frontend/src/Components/OutcomeMessage.jsx
@@ -4,14 +4,14 @@ import { FiAlertTriangle, FiCheckSquare } from "react-icons/fi";
 function OutcomeMessage({ outcome, content }) {
 	if (outcome == "success") {
 		return (
-			<div className="absolute  bottom-14 flex items-center rounded-lg bg-secondaryBg p-4 space-x-2">
+			<div className="absolute bottom-14 transform translate-y-1/2 left-1/2 -translate-x-1/2 flex items-center rounded-lg bg-secondaryBg p-4 space-x-2">
 				<FiCheckSquare className=" text-green-500" />
 				<p className="text-white">{content}</p>
 			</div>
 		);
 	} else {
 		return (
-			<div className="absolute  bottom-14 flex items-center rounded-lg bg-secondaryBg p-4 space-x-2">
+			<div className="absolute bottom-14 transform translate-y-1/2 left-1/2 -translate-x-1/2 flex items-center rounded-lg bg-secondaryBg p-4 space-x-2">
 				<FiAlertTriangle className=" text-red-500" />
 				<p className="text-white">{content}</p>
 			</div>
