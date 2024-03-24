6d434cebd3ee4a7613497cc797a9e132c79056e8
Johan Nyman <johan.nyman97@gmail.com>
2024-03-24 23:23:04 +0100

    Stor bokstav på title.

--- frontend/src/Components/Task.jsx
+++ frontend/src/Components/Task.jsx
@@ -5,7 +5,7 @@ function Task({ task }) {
 	return (
 		<div className="flex flex-col bg-secondaryBg rounded-lg min-h-40 max-w-48 p-4 mt-4 h-full justify-between">
 			<div>
-				<p className=" text-lg">{task.title}</p>
+				<p className=" text-lg capitalize">{task.title}</p>
 				<span className=" text-gray-500 text-base">{task.description}</span>
 			</div>
 			<div className=" text-lg flex justify-between items-center">
