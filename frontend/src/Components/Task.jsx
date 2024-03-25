3a10f13d4ebb89410e6ff685dc423cea35f8f6dd
Johan Nyman <johan.nyman97@gmail.com>
2024-03-25 02:08:23 +0100

    tar in props. conditional för complete eller inte

--- frontend/src/Components/Task.jsx
+++ frontend/src/Components/Task.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import { FiX, FiChevronsRight } from "react-icons/fi";
-function Task({ task }) {
+import { FiX, FiChevronsRight, FiCheck } from "react-icons/fi";
+function Task({ task, deleteTask, updateStatus }) {
 	return (
 		<div className="flex flex-col bg-secondaryBg rounded-lg h-fit max-w-48 p-4 mt-4 justify-between">
 			<div className="mb-4">
@@ -9,12 +9,33 @@ function Task({ task }) {
 				<span className=" text-gray-500 text-base">{task.description}</span>
 			</div>
 			<div className=" text-lg flex justify-between items-center">
-				<button className=" text-red-500">
+				<button
+					className=" text-red-500"
+					onClick={(e) => {
+						deleteTask(task.id);
+					}}
+				>
 					<FiX />
 				</button>
-				<button className=" text-green-500">
-					<FiChevronsRight />
-				</button>
+				{task.status !== 3 ? (
+					<button
+						className="text-green-500"
+						onClick={(e) => {
+							updateStatus(task.id, task.status + 1);
+						}}
+					>
+						<FiChevronsRight />
+					</button>
+				) : (
+					<button
+						className="text-green-500"
+						onClick={(e) => {
+							deleteTask(task.id);
+						}}
+					>
+						<FiCheck />
+					</button>
+				)}
 			</div>
 		</div>
 	);
